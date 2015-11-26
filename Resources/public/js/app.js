2914c7b89c83ab1bb5b5a17eba8551eb1b40e914
Matthieu VIDEAUD <matthieu@tctf.fr>
2015-11-26 19:29:38 +0100

    add message on image more than 1Mo

--- Resources/public/js/app.js
+++ Resources/public/js/app.js
@@ -9,7 +9,15 @@ jQuery(function($) {
         },5000);
 
     }
-    
+
+    function FileConvertSize(aSize){
+        aSize = Math.abs(parseInt(aSize, 10));
+        var def = [[1, 'octets'], [1024, 'ko'], [1024*1024, 'Mo'], [1024*1024*1024, 'Go'], [1024*1024*1024*1024, 'To']];
+        for(var i=0; i<def.length; i++){
+            if(aSize<def[i][0]) return (aSize/def[i-1][0]).toFixed(2)+' '+def[i-1][1];
+        }
+    }
+
     /**
      * Gestion bouton upload
      */
@@ -20,6 +28,9 @@ jQuery(function($) {
         var f = e.target.files[0].name;
         if(f.length > 30) f = f.substr(0,27)+'...';
         btn.parent().parent().find('.text-file').val(f);
+        if(e.target.files[0].size > 1000000) {
+            alert('Attention votre image n\'est certainement pas adapté pour un format web. Poids maximum recommandé 1Mo, votre image pèse '+FileConvertSize(e.target.files[0].size));
+        }   
     });
 
     $('.sidebar-menu>li').each(function(){
